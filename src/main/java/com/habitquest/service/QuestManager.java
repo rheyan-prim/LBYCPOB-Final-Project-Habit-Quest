b020d7f026842484720f80b0d437ce1bf4003874
rheyan-prim <rheyan_prim@dlsu.edu.ph>
2026-08-16 19:41:03 +0800

    Created QuestManager.java

---
+++ src/main/java/com/habitquest/service/QuestManager.java
@@ -0,0 +1,6 @@
+package com.habitquest.service;
+
+import com.habitquest.model.Habit;
+import com.habitquest.model.Rewardable;
+import com.habitquest.model.User;
+import org.springframework.stereotype.Component;

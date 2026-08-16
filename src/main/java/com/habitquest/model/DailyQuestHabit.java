94db16ee20ff0bca4819fa8dfe328118a134b047
rafaeldiongco <rafael_diongco@dlsu.edu.ph>
2026-08-16 15:29:25 +0800

    Added skeleton of DailyQuestHabit(TODO)

--- src/main/java/com/habitquest/model/DailyQuestHabit.java
+++ src/main/java/com/habitquest/model/DailyQuestHabit.java
@@ -1,4 +1,28 @@
 package com.habitquest.model;
 
-public class DailyQuestHabit {
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("DAILY")
+public class DailyQuestHabit extends Habit implements Rewardable {
+
+    protected DailyQuestHabit() {
+        super();
+    }
+
+    public DailyQuestHabit(String title, String description, String imagePath) {
+        super(title, description, imagePath);
+    }
+
+    @Override
+    public int calculateXPReward() {
+        return 10; // flat rate placeholder, refine later with streak multiplier
+    }
+
+    @Override
+    public void grantReward(User user) {
+        // TODO: wire into LevelingSystem once it exists
+        user.gainXP(calculateXPReward());
+    }
 }

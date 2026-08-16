47e4a1d2df16391054f221d1f5ce8f994afdff31
rafaeldiongco <rafael_diongco@dlsu.edu.ph>
2026-08-16 15:32:48 +0800

    Partial implementation of ToDoQuestHabit

--- src/main/java/com/habitquest/model/ToDoQuestHabit.java
+++ src/main/java/com/habitquest/model/ToDoQuestHabit.java
@@ -1,4 +1,27 @@
 package com.habitquest.model;
 
-public class ToDoQuestHabit {
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("TODO")
+public class ToDoQuestHabit extends Habit implements Rewardable {
+
+    protected ToDoQuestHabit() {
+        super();
+    }
+
+    public ToDoQuestHabit(String title, String description, String imagePath) {
+        super(title, description, imagePath);
+    }
+
+    @Override
+    public int calculateXPReward() {
+        return 50; // one-time "epic quest" placeholder, higher than daily
+    }
+
+    @Override
+    public void grantReward(User user) {
+        user.gainXP(calculateXPReward());
+    }
 }

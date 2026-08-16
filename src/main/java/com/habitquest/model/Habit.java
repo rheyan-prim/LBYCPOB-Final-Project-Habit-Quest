29c30e8351f1e32d4ccc8bf4ad86827e896df6f3
rafaeldiongco <rafael_diongco@dlsu.edu.ph>
2026-08-16 15:22:18 +0800

    Partial implementation of Habit.java

--- src/main/java/com/habitquest/model/Habit.java
+++ src/main/java/com/habitquest/model/Habit.java
@@ -1,4 +1,28 @@
 package com.habitquest.model;
 
-public class Habit {
-}
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "habit_type")
+public abstract class Habit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String imagePath;
+    private boolean completionStatus;
+
+    protected Habit() {
+        // required by JPA
+    }
+
+    public Habit(String title, String description, String imagePath) {
+        this.title = title;
+        this.description = description;
+        this.imagePath = imagePath;
+        this.completionStatus = false;
+    }

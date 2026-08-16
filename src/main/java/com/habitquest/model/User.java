60e6db8280a791ab87a5e48095c5ac7fd004c55f
rafaeldiongco <rafael_diongco@dlsu.edu.ph>
2026-08-16 15:41:00 +0800

    Partial implementation of User.java

--- src/main/java/com/habitquest/model/User.java
+++ src/main/java/com/habitquest/model/User.java
@@ -1,4 +1,32 @@
 package com.habitquest.model;
 
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class User {
-}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password; // hashed later by Person 3's auth flow
+
+    private int totalXP;
+    private int currentLevel;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Habit> habits = new ArrayList<>();
+
+    protected User() {
+        // required by JPA
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.totalXP = 0;
+        this.currentLevel = 1;
+    }

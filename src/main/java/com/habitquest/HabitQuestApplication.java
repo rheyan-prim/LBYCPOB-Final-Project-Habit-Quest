3a09ab960f80e7ac9aa9fce4bceb3c1443651cb4
rafaeldiongco <rafael_diongco@dlsu.edu.ph>
2026-08-16 15:48:45 +0800

    Added main application class

---
+++ src/main/java/com/habitquest/HabitQuestApplication.java
@@ -0,0 +1,12 @@
+package com.habitquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitQuestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitQuestApplication.class, args);
+    }
+}

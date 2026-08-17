c6b004c1a952be9322fc3fc47d863e833095286a
hailegabriel-chico <haile_chico@dlsu.edu.ph>
2026-08-17 10:08:16 +0800

    HabitController -- added getUserHabits

--- src/main/java/com/habitquest/controller/HabitController.java
+++ src/main/java/com/habitquest/controller/HabitController.java
@@ -1,7 +1,11 @@
 package com.habitquest.controller;
+import com.habitquest.model.Habit;
+import com.habitquest.model.User;
 import com.habitquest.repository.HabitRepository;
 import com.habitquest.repository.UserRepository;
 import com.habitquest.service.QuestManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -14,6 +18,14 @@ public class HabitController {
         this.habitRepository = habitRepository;
         this.userRepository = userRepository;
         this.questManager = questManager;
+    }
+
+    // Fetch all habits for a specific user
+    @GetMapping
+    public List<Habit> getUserHabits(@RequestParam Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        return user.getHabits();
     }
 
 }

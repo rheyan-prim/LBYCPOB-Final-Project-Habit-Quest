95ff35ac496a3ff407c2a96a2ea8a667620501fc
hailegabriel-chico <haile_chico@dlsu.edu.ph>
2026-08-17 09:26:23 +0800

    Added HabitRepository.java

---
+++ src/main/java/com/habitquest/repository/HabitRepository.java
@@ -0,0 +1,7 @@
+package com.habitquest.repository;
+
+import com.habitquest.model.Habit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HabitRepository extends JpaRepository<Habit, Long> {
+}

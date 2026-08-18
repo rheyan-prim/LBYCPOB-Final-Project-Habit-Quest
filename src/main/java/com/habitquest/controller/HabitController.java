d09980c5fab8a3f4961acff726294aca3e9a839b
rafaeldiongco <rafael_diongco@dlsu.edu.ph>
2026-08-18 16:07:53 +0800

    Fixed HabitController missing @RestController and @RequestMapping annotations

--- src/main/java/com/habitquest/controller/HabitController.java
+++ src/main/java/com/habitquest/controller/HabitController.java
@@ -10,8 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+
 
 import java.util.List;
+@RestController
+@RequestMapping("/api/habits")
 
 public class HabitController {
     private final HabitRepository habitRepository;
